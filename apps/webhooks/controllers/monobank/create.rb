d5dcfdd1555e459ba5b182ea5033362b1d2914df
Nazar Vinnychuk <ux.isco@gmail.com>
2021-04-24 15:50:08 +0300

    Support monobank updates through webhook

--- apps/webhooks/controllers/monobank/create.rb
+++ apps/webhooks/controllers/monobank/create.rb
@@ -4,10 +4,49 @@ module Webhooks
       class Create
         include Webhooks::Action
 
+        using CoreExt::Hash
+        using CoreExt::String
+
         accept :json
 
+        def initialize(accounts:        AccountRepository.new,
+                       statement_items: StatementItemRepository.new)
+          @accounts        = accounts
+          @statement_items = statement_items
+        end
+
+        # {
+        #   "type"=>"StatementItem",
+        #   "data"=>{
+        #     "account"=>"xxxxxx",
+        #     "statementItem"=>{
+        #       "id"=>"xxxxxxxx",
+        #       "time"=>1619266478,
+        #       "description"=>"На Білу картку",
+        #       "mcc"=>4829,
+        #       "originalMcc"=>4829,
+        #       "amount"=>-104,
+        #       "operationAmount"=>-104,
+        #       "currencyCode"=>980,
+        #       "commissionRate"=>4,
+        #       "cashbackAmount"=>0,
+        #       "balance"=>2865221,
+        #       "hold"=>true
+        #     }
+        #   }
+        # }
+
         def call(params)
-          Hanami.logger.debug(params.raw)
+          return status 200, 'Unsupported type' if params[:type] != 'StatementItem'
+
+          data = params.to_h.deep_transform_keys { |key| key.to_s.underscore.to_sym }[:data]
+
+          statement_item = @statement_items.class.entity.new(data[:statement_item])
+
+          account = @accounts.update(data[:account], balance: statement_item.balance)
+          @statement_items.sync(account, statement_item)
+
+          status 200, 'OK'
         end
       end
     end
